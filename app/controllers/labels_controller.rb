4837121779b72942756573abc48ff2be4b22050c
y-horie517 <y.horie0517@gmail.com>
2019-03-12 11:21:36 +0000

    [Update]エラーメッセージ出力

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,19 @@
 class LabelsController < ApplicationController
+	def create
+		@label = Label.new(label_params)
+
+		if @label.save
+			flash[:notice] = "レーベルが１件登録されました。"
+			redirect_to master_new_path
+		else
+			flash[:warning] = "レーベルの登録に失敗しました。"
+			render master_new_path
+		end
+	end
+
+	private
+
+	def label_params
+			params.require(:label).permit(:label_name)
+	end
 end
--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,2 +1,19 @@
 class MusicsController < ApplicationController
+	def create
+		@music = Music.new(music_params)
+
+		if @music.save
+			flash[:notice] = "楽曲が１件登録されました。"
+			redirect_to master_new_path
+		else
+			flash[:warning] = "楽曲の登録に失敗しました。"
+			render master_new_path
+		end
+	end
+
+	private
+
+	def music_params
+			params.require(:music).permit(:music_name, :artist_id)
+	end
 end
