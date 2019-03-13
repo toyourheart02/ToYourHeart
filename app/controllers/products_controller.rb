54c0dbec544ea98f2dd4c1411ad2c4bd0855649e
Lilysss02 <shkrkmtd219@gmail.com>
2019-03-13 09:16:25 +0000

    [Add]商品一覧ページ機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,12 @@ class ProductsController < ApplicationController
 	def index
 		# config/initializers/kaminari_configで1ページの表示件数20件に設定
 		@products = Product.page(params[:page]).reverse_order
+		# 新着10点の画像をスライドショーで表示
+		@product_topten = Product.limit(10).order('release_date')
 	end
+
+	def search
+
+	end
+
 end
