723ebdc6e36238ce711e354a690a7036aebe238c
Lilysss02 <shkrkmtd219@gmail.com>
2019-03-13 04:21:57 +0000

    [Create]管理者ログイン、Topページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_admin!
+
+	def after_sign_in_path_for(resource)
+		admins_path
+	end
+
+	def after_sign_out_path_for(resource)
+		new_admin_session_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'products' => 'products#index'
   root 'products#index'
+  get 'admins' => 'admins#top'
 
 end
