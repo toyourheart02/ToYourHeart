60a7d81a63d21bc4e6000c9eca9b5abbe4325b49
y-horie517 <y.horie0517@gmail.com>
2019-03-14 09:27:25 +0000

    [Create]マイページ＆ユーザ情報編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,29 @@ class UsersController < ApplicationController
 
 	#ユーザのマイページ用に作成。管理者からの機能は後ほど追加。
 	def show
-	  	# @user = current_user
 	  	@user = User.find(params[:id])
   	end
 
     def edit
+    	@user = User.find(params[:id])
     	
     end
 
     def update
-    	
+    	user = User.find(params[:id])
+	  	if user.update(user_params)
+	      flash[:notice] = 'ユーザ情報が更新されました。'
+	      redirect_to user_path(user.id)
+	    else
+	      flash[:warning] = 'ユーザ情報の更新に失敗しました。'
+	      redirect_to edit_user_path(user.id)
+    	end
     end
+
+
+    private
+	
+	def user_params
+		params.require(:user).permit(:user_name, :user_kana, :zip_code, :address, :phone_number, :profile_image)
+	end
 end
