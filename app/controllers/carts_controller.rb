918737d8a31b1fe544d2c60b5ceff7120479df4a
y-horie517 <y.horie0517@gmail.com>
2019-03-17 11:04:20 +0000

    [Add]カート追加処理の非同期化＆追加済み商品に対する個数処理

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,13 +8,23 @@ class CartsController < ApplicationController
 
   def create
   	cart = Cart.new
+  	user = current_user
+
   	# user_idはcurrent_userのidを代入
   	cart.user_id = current_user.id
   	#product_idはクリックされた商品のproduct_idを代入
   	cart.product = Product.find(params[:product_id])
-  	cart.save
-  	# 他の画面でも使用できるようにリダイレクトは非同期処理にしたい。（それぞれの画面のまま）
-  	redirect_to products_path
+
+  	# カート内にuser, productの組み合わせを検索して変数へ代入
+  	added_cart = user.carts.find_by(product_id: cart.product.id)
+  	# 存在しなければ新たにカートに追加
+  	if  added_cart == nil
+  		cart.save
+  	# 存在する場合は、quantityを＋１する
+  	else
+  		added_cart.quantity += 1
+  		added_cart.save
+  	end
   end
 
 # カート内の商品を削除するアクション
@@ -28,5 +38,20 @@ class CartsController < ApplicationController
 		flash[:notice] = "カート内商品を１件削除しました。"
 		redirect_to carts_path
 	end
+  end
+# 個数変更
+  def update
+  	# 更新したい商品を特定
+  	user = current_user
+  	product = Product.find(params[:id])
+  	# cartsテーブル内で個数更新するカラムを指定(１件取得のためfind_by)
+  	cart = user.carts.find_by(product_id: product.id)
+	cart.quantity = 2
+	cart.update
+	redirect_to carts_path
+  end
+
+  def cart_params
+			params.require(:cart).permit(:quantity)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :scene
   belongs_to :genre
   belongs_to :label
+  belongs_to :artist
   attachment :music_image
   has_many :carts, dependent: :destroy
   has_many :users, through: :carts
