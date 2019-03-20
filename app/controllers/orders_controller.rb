59b9bb3d3ae2f50681b3fa3ff1908fc256cdfd75
y-horie517 <y.horie0517@gmail.com>
2019-03-20 07:27:34 +0000

    [Create]注文機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,30 +11,43 @@ class OrdersController < ApplicationController
 	def create
 		# 注文登録処理
 		order = Order.new(order_params)
-		binding.pry
 		# orderテーブルの各カラムに情報を格納
 		order.user_id = current_user.id
-		#配送先は仮
+		#配送先は仮で1番目が選ばれる
 		dests = current_user.destinations
 	    order.destination_id = dests[0].id
 	    # payはフォームから取得済み
 	    order.save
 
-
 	    # 注文済み商品登録処理
-	    order_products = []
-
-	    # @products.each do |product|
-	    # 	OrderProduct.new
-	    # end
-
 	    # 現在のユーザのカート内レコードを取得
 	    cart = Cart.where(user_id: current_user.id)
+	    #取得したカートのレコード一件ごとに処理する。注文数と在庫数を比較
+	    cart.each do |cart|
+	    	# 商品の在庫数と注文数を比較して注文可能なら処理を行う。
+		     if cart.product.stock > cart.quantity
+		     	orderproduct = OrderProduct.new
+		     	orderproduct.product_id = cart.product_id
+		     	orderproduct.quantity = cart.quantity
+		     	orderproduct.price = cart.product.price
+		     	orderproduct.order_id = order.id
+		     	
+		     	# 在庫数を更新(注文数を引く)
+		     	cart.product.stock -= cart.quantity
+		     	cart.product.save
 
+				# 注文商品を登録
+				orderproduct.save
 
-	    # カート内商品削除処理
-	    #取得したレコードをすべて削除
-	    cart.destroy_all(id > 0)
+		     	# 登録が成功した商品はカートから削除（在庫の関係で注文エラーになったものはカートに残す）
+	     		cart.destroy
+	     		flash[:notice] = "商品の購入が完了しました。購入履歴ページで確認してください。"
+	     	# 商品の在庫数と注文数を比較して注文不可なら。warningにメッセージを入れておく
+	     	else
+	     		flash[:warning] = "購入できなかった商品があります。在庫数を確認して注文個数を変更してください。"
+	     	end
+	    end
+	    redirect_to carts_path
 	end
 
 	private
